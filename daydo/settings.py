7383701261b218121d27b1aa8a1bef8047e269d7
harshithtunuguntla <harshithtunuguntla@gmail.com>
2021-12-31 01:36:31 +0530

    added whitenoise middleware

--- daydo/settings.py
+++ daydo/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2%a+tw$utf6-g2ylnb#%9$(3djbea*5#$nzyu3i6ph4_ukju_!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['harshithtodo.herokuapp.com']
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
