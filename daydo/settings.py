220366c59e8ec40258166628bc7cafcef752ed02
Harshith Sai Tunuguntla <harshith.tunuguntla.19cse@bmu.edu.in>
2023-02-07 21:55:48 +0530

    changes in csrf

--- daydo/settings.py
+++ daydo/settings.py
@@ -25,15 +25,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2%a+tw$utf6-g2ylnb#%9$(3djbea*5#$nzyu3i6ph4_ukju_!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://harshithtodo.up.railway.app/']
 
-# CSRF_TRUSTED_ORIGINS = ["https://harshithtodo.up.railway.app/"]
+CSRF_TRUSTED_ORIGINS = ["https://harshithtodo.up.railway.app/"]
 # Application definition
 
 INSTALLED_APPS = [
     'tasks',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
      'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
